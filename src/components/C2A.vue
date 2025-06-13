3c74c6b662f0c414c3be45fefe30e64161c308ce
Aiden Redmond <adnrdmnd@gmail.com>
2025-06-13 17:00:47 -0400

    link it on the sign up thing too

--- src/components/C2A.vue
+++ src/components/C2A.vue
@@ -4,14 +4,15 @@
       Start working towards <br />your eclipse today.
     </p>
     <div class="flex items-center justify-center mt-12 gap-4">
-      <RouterLink
-        to="/training"
-        class="bg-red text-white px-6 py-3 rounded-md hover:bg-light-red transition text-xs sm:text-md"
-        >Sign up Today</RouterLink
+      <a
+        href="https://docs.google.com/forms/d/e/1FAIpQLSf3dqDY81DB53QyXEpeQZFCJeOtyKKGMHXKIefxzkG38eHgtA/viewform?usp=sharing&ouid=110534330143953080548"
+        target="_blank"
+        class="bg-red text-white px-6 py-3 rounded-md hover:bg-light-red transition text-xs sm:text-base"
+        >Sign up Today</a
       >
       <RouterLink
         to="/contact"
-        class="border border-red text-red px-6 py-3 rounded-md hover:bg-light-red hover:text-white transition text-xs sm:text-md"
+        class="border border-red text-red px-6 py-3 rounded-md hover:bg-light-red hover:text-white transition text-xs sm:text-base"
         >Get in Touch</RouterLink
       >
     </div>
